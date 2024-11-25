347a6e411e4a7beeb307d48c48c9a905064b7242
leecoading <leehy011011@gmail.com>
2024-11-25 19:23:06 +0900

    [Add] Entity, Player
    Entity, Player 클래스 추가

---
+++ Invisiphobia_NullPath/Assets/Scripts/Entity/Entity.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    /// <summary>
+    /// Entity 초기화 함수
+    /// </summary>
+    public virtual void Init()
+    {
+
+    }
+}
---
+++ Invisiphobia_NullPath/Assets/Scripts/Entity/Player/Player.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+
+    public override void Init()
+    {
+        base.Init();
+    }
+
+
+}
