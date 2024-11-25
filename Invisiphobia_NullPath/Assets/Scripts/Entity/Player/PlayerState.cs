5c956e29378435d82e4bc00111035b09d13beaec
leecoading <leehy011011@gmail.com>
2024-11-25 19:27:13 +0900

    [Add] PlayerController, PlayerState
    PlayerController, PlayerState 클래스
    추가

---
+++ Invisiphobia_NullPath/Assets/Scripts/Entity/Player/PlayerState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : Entity
+{
+    public override void Init()
+    {
+        base.Init();
+    }
+}
