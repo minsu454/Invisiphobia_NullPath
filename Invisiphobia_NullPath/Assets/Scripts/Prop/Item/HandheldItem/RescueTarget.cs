f21008ab89db3efb04c8b227851f9af34c3054a8
keubung <skdud3390@naver.com>
2024-11-26 17:36:45 +0900

    [Fix] RescueTarget

--- Invisiphobia_NullPath/Assets/Scripts/Prop/Item/HandheldItem/RescueTarget.cs
+++ Invisiphobia_NullPath/Assets/Scripts/Prop/Item/HandheldItem/RescueTarget.cs
@@ -2,11 +2,6 @@ public class RescueTarget : HandheldItem
 {
     public bool IsRescueTarget { get; private set; }
 
-    public RescueTarget(int id, string name, string description, bool isRescueTarget) : base(id, name, description)
-    {
-        IsRescueTarget = isRescueTarget;
-    }
-
     public override void Interact()
     {
     }
