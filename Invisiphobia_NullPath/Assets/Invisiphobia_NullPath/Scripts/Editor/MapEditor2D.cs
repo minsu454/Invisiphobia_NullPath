610b651a9dfcf9d4c9b509c79e77330e27731458
minsu454 <kminsu454@gmail.com>
2024-11-28 05:45:01 +0900

    [Update] MapEditor
    기즈모를 활용해서 설치박스 그리기

--- Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Editor/MapEditor2D.cs
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Editor/MapEditor2D.cs
@@ -1,150 +1,150 @@
-using UnityEditor;
-using UnityEditor.SceneManagement;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
+//using UnityEditor;
+//using UnityEditor.SceneManagement;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
+//using UnityEngine.UIElements;
 
-public class MapEditor2D : EditorWindow
-{
-    private static MapEditor2D window;
-    private bool isCreateData = false;
-    private bool isCreateMap = false;
+//public class MapEditor2D : EditorWindow
+//{
+//    private static MapEditor2D window;
+//    private bool isCreateData = false;
+//    private bool isCreateMap = false;
 
-    private GameObject map;
-    private Vector2 mapSize;
+//    private GameObject map;
+//    private Vector2 mapSize;
 
-    private GameObject background;
-    private GameObject plane;
+//    private GameObject background;
+//    private GameObject plane;
 
-    private string brforeScenePath; // 현재 씬 저장용
-    private Scene brforeScene;     // 임시 씬
-    private const string UseScenePath = "Assets/Invisiphobia_NullPath/Scenes/MapEditor.unity";
-    private const string planePath = "Assets/Invisiphobia_NullPath/Prefabs/Map/Plane.prefab";
+//    private string brforeScenePath; // 현재 씬 저장용
+//    private Scene brforeScene;     // 임시 씬
+//    private const string UseScenePath = "Assets/Invisiphobia_NullPath/Scenes/MapEditor.unity";
+//    private const string planePath = "Assets/Invisiphobia_NullPath/Prefabs/Map/Plane.prefab";
 
 
-    [MenuItem("Tools/MapEditor/2DMap")]
-    public static void Init()
-    {
-        if (window != null)
-        {
-            return;
-        }
+//    [MenuItem("Tools/MapEditor/2DMap")]
+//    public static void Init()
+//    {
+//        if (window != null)
+//        {
+//            return;
+//        }
 
-        window = GetWindow<MapEditor2D>("Create 2D Map");
-        window.Show();
+//        window = GetWindow<MapEditor2D>("Create 2D Map");
+//        window.Show();
 
-        //// 최소, 최대 크기 지정
-        window.minSize = new Vector2(340f, 100f);
-        window.maxSize = new Vector2(500f, 1000f);
-    }
+//        //// 최소, 최대 크기 지정
+//        window.minSize = new Vector2(340f, 100f);
+//        window.maxSize = new Vector2(500f, 1000f);
+//    }
 
-    private void OnGUI()
-    {
-        // Normal =====================================================================
-        GUILayout.Space(10f);
-        GUILayout.Label("Normal", EditorStyles.boldLabel);
+//    private void OnGUI()
+//    {
+//        // Normal =====================================================================
+//        GUILayout.Space(10f);
+//        GUILayout.Label("Normal", EditorStyles.boldLabel);
 
-        GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Map Create"))
-        {
-            Create2DMap();
-        }
+//        GUILayout.BeginHorizontal();
+//        if (GUILayout.Button("Map Create"))
+//        {
+//            Create2DMap();
+//        }
 
-        if (GUILayout.Button("Map Delete"))
-        {
-            Delete2DMap();
-        }
-        GUILayout.EndHorizontal();
+//        if (GUILayout.Button("Map Delete"))
+//        {
+//            Delete2DMap();
+//        }
+//        GUILayout.EndHorizontal();
 
-        if (!isCreateData)
-            return;
+//        if (!isCreateData)
+//            return;
 
-        // Normal =====================================================================
-        GUILayout.Space(10f);
+//        // Normal =====================================================================
+//        GUILayout.Space(10f);
 
-        mapSize = EditorGUILayout.Vector2Field("Map Size", mapSize);
-        if (GUILayout.Button("Bake"))
-        {
-            if (mapSize.x <= 0 || mapSize.x % 1 != 0 || mapSize.y <= 0 || mapSize.y % 1 != 0)
-            {
-                Debug.LogWarning("Map Size Value is OutOfRange. (integer, Positive Number)");
-                return;
-            }
+//        mapSize = EditorGUILayout.Vector2Field("Map Size", mapSize);
+//        if (GUILayout.Button("Bake"))
+//        {
+//            if (mapSize.x <= 0 || mapSize.x % 1 != 0 || mapSize.y <= 0 || mapSize.y % 1 != 0)
+//            {
+//                Debug.LogWarning("Map Size Value is OutOfRange. (integer, Positive Number)");
+//                return;
+//            }
 
-            if (background == null)
-            {
-                background = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                background.transform.SetParent(map.transform);
-                background.transform.position = new Vector3(0, -1, 0);
+//            if (background == null)
+//            {
+//                background = GameObject.CreatePrimitive(PrimitiveType.Cube);
+//                background.transform.SetParent(map.transform);
+//                background.transform.position = new Vector3(0, -1, 0);
 
-                plane = Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>(planePath));
-                plane.transform.position = new Vector3(0, -0.49f, 0);
-                plane.transform.SetParent(map.transform);
-            }
+//                plane = Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>(planePath));
+//                plane.transform.position = new Vector3(0, -0.49f, 0);
+//                plane.transform.SetParent(map.transform);
+//            }
 
-            background.transform.localScale = new Vector3(mapSize.x, 1, mapSize.y);
-            plane.transform.localScale = new Vector3(mapSize.x / 10, 1, mapSize.y / 10);
+//            background.transform.localScale = new Vector3(mapSize.x, 1, mapSize.y);
+//            plane.transform.localScale = new Vector3(mapSize.x / 10, 1, mapSize.y / 10);
 
-            isCreateMap = true;
-        }
-    }
+//            isCreateMap = true;
+//        }
+//    }
 
-    /// <summary>
-    /// 2D맵 생성 함수
-    /// </summary>
-    private void Create2DMap()
-    {
-        if (map == null)
-        {
-            if (brforeScene.IsValid())
-            {
-                Debug.LogWarning("이미 맵 편집 씬이 열려있습니다.");
-                return;
-            }
+//    /// <summary>
+//    /// 2D맵 생성 함수
+//    /// </summary>
+//    private void Create2DMap()
+//    {
+//        if (map == null)
+//        {
+//            if (brforeScene.IsValid())
+//            {
+//                Debug.LogWarning("이미 맵 편집 씬이 열려있습니다.");
+//                return;
+//            }
 
-            brforeScenePath = EditorSceneManager.GetActiveScene().path;
-            brforeScene = EditorSceneManager.OpenScene(UseScenePath);
+//            brforeScenePath = EditorSceneManager.GetActiveScene().path;
+//            brforeScene = EditorSceneManager.OpenScene(UseScenePath);
 
-            map = new GameObject("Map");
-            Debug.Log("Create Completed");
+//            map = new GameObject("Map");
+//            Debug.Log("Create Completed");
 
-            isCreateData = true;
-        }
-        else
-        {
-            Debug.LogWarning("map has already been created.");
-        }
-    }
+//            isCreateData = true;
+//        }
+//        else
+//        {
+//            Debug.LogWarning("map has already been created.");
+//        }
+//    }
 
-    /// <summary>
-    /// 2D맵 삭제 함수
-    /// </summary>
-    private void Delete2DMap()
-    {
-        if (map != null)
-        {
-            Debug.Log("Delete Completed");
-            DestroyImmediate(map);
+//    /// <summary>
+//    /// 2D맵 삭제 함수
+//    /// </summary>
+//    private void Delete2DMap()
+//    {
+//        if (map != null)
+//        {
+//            Debug.Log("Delete Completed");
+//            DestroyImmediate(map);
 
-            isCreateData = false;
-            isCreateMap = false;
+//            isCreateData = false;
+//            isCreateMap = false;
 
-            if (!brforeScene.IsValid())
-                return;
+//            if (!brforeScene.IsValid())
+//                return;
 
-            // 임시 씬 닫기
-            EditorSceneManager.OpenScene(brforeScenePath);
+//            // 임시 씬 닫기
+//            EditorSceneManager.OpenScene(brforeScenePath);
 
-        }
-        else
-        {
-            Debug.LogWarning("map has already been deleted.");
-        }
-    }
+//        }
+//        else
+//        {
+//            Debug.LogWarning("map has already been deleted.");
+//        }
+//    }
 
-    private void OnDisable()
-    {
-        if (map != null)
-            Delete2DMap();
-    }
-}
+//    private void OnDisable()
+//    {
+//        if (map != null)
+//            Delete2DMap();
+//    }
+//}
