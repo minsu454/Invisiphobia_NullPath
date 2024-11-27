c662ac799858284e395bbf2b1528c9298dd6410f
keubung <skdud3390@naver.com>
2024-11-27 15:39:13 +0900

    [Update] MonsterController.cs
    AI Navigation install

--- Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Entity/Monster/MonsterController.cs
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Entity/Monster/MonsterController.cs
@@ -1,8 +1,179 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
+public enum AIState
+{
+    Idle,
+    Wandering,
+    Attacking
+}
 
 public class MonsterController : Entity
 {
-    
+    [Header("Stats")]
+    public float walkSpeed;
+    public float runSpeed;
+
+    [Header("AI")]
+    public float detectDistance;
+    private NavMeshAgent agent;
+    private AIState aiState;
+
+    [Header("Wandering")]
+    public float minWanderDistance;
+    public float maxWanderDistance;
+    public float minWanderWaitTime;
+    public float maxWanderWaitTime;
+
+    [Header("Combat")]
+    public float attackDistance;
+
+    private float playerDistance;
+
+    public float fieldOfView = 120f;
+
+    public Transform playerTransform;
+
+    //private Animator animator;
+    //private SkinnedMeshRenderer[] meshRenderers;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //animator = GetComponent<Animator>();
+        //meshRenderers = GetComponentsInChildren<SkinnedMeshRenderer>();
+        SetState(AIState.Idle);
+    }
+
+    void Update()
+    {
+        playerDistance = Vector3.Distance(transform.position, playerTransform.position);
+
+        //animator.SetBool("Moving", aiState != AIState.Idle);
+
+        switch (aiState)
+        {
+            case AIState.Idle:
+            case AIState.Wandering:
+                break;
+            case AIState.Attacking:
+                break;
+        }
+    }
+
+    public void SetState(AIState state)
+    {
+        aiState = state;
+
+        switch (aiState)
+        {
+            case AIState.Idle:
+                agent.speed = walkSpeed;
+                agent.isStopped = true;
+                break;
+            case AIState.Wandering:
+                agent.speed = walkSpeed;
+                agent.isStopped = false;
+                break;
+            case AIState.Attacking:
+                agent.speed = runSpeed;
+                agent.isStopped = false;
+                break;
+        }
+
+        //animator.speed = agent.speed / walkSpeed;
+    }
+
+    void PassiveUpdate()
+    {
+        if (aiState == AIState.Wandering && agent.remainingDistance < 0.1f)
+        {
+            SetState(AIState.Idle);
+            Invoke("WanderToNewLocation", Random.Range(minWanderWaitTime, maxWanderWaitTime));
+        }
+
+        if (playerDistance < detectDistance)
+        {
+            SetState(AIState.Attacking);
+        }
+    }
+
+    void AttackingUpdate()
+    {
+        if (playerDistance > attackDistance || !IsPlayerInFieldOfView())
+        {
+            agent.isStopped = false;
+            NavMeshPath path = new NavMeshPath();
+            if (agent.CalculatePath(playerTransform.position, path))
+            {
+                agent.SetDestination(playerTransform.position);
+            }
+            else
+            {
+                //SetState(AIState.Fleeing);
+            }
+        }
+        else
+        {
+            agent.isStopped = true;
+            //playerController.GetComponent<IDamagable>().TakePhysicalDamage(damage);
+            //animator.speed = 1;
+            //animator.SetTrigger("Attack");
+
+        }
+    }
+
+    void FleeingUpdate()
+    {
+        if (agent.remainingDistance < 0.1f)
+        {
+           // agent.SetDestination(GetFleeLocation());
+        }
+        else
+        {
+            SetState(AIState.Wandering);
+        }
+    }
+
+    void WanderToNewLocation()
+    {
+        if (aiState != AIState.Idle)
+        {
+            return;
+        }
+        SetState(AIState.Wandering);
+        agent.SetDestination(GetWanderLocation());
+    }
+
+    bool IsPlayerInFieldOfView()
+    {
+        Vector3 directionToPlayer = playerTransform.position - transform.position;
+        float angle = Vector3.Angle(transform.forward, directionToPlayer);
+        return angle < fieldOfView * 0.5f;
+    }
+
+    Vector3 GetWanderLocation()
+    {
+        NavMeshHit hit;
+
+        NavMesh.SamplePosition(transform.position + (Random.onUnitSphere * Random.Range(minWanderDistance, maxWanderDistance)), out hit, maxWanderDistance, NavMesh.AllAreas);
+
+        int i = 0;
+        while (Vector3.Distance(transform.position, hit.position) < detectDistance)
+        {
+            NavMesh.SamplePosition(transform.position + (Random.onUnitSphere * Random.Range(minWanderDistance, maxWanderDistance)), out hit, maxWanderDistance, NavMesh.AllAreas);
+            i++;
+            if (i == 30)
+                break;
+        }
+
+        return hit.position;
+    }
+
+    float GetDestinationAngle(Vector3 targetPos)
+    {
+        return Vector3.Angle(transform.position - playerTransform.position, transform.position + targetPos);
+    }
 }
