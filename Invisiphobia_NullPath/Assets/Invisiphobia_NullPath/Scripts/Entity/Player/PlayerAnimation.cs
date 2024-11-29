60e6ff065522d8e1d3d317c07502c3064473f9a4
leecoading <leehy011011@gmail.com>
2024-11-29 12:33:22 +0900

    [Add] PlayerAnimation

---
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Entity/Player/PlayerAnimation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    
+
+    private void OnPlayerMove()
+    {
+
+    }
+
+    private void OnPlayerRun()
+    {
+
+    }
+
+}
