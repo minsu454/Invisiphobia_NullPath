e4bd8aebd8357c628161b04da12891fd65af2218
minsu454 <kminsu454@gmail.com>
2024-12-01 03:48:27 +0900

    [Add] MapSaveManager
    MapSaveManager 준비

---
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Editor/MapSaveManager.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class MapSaveManager
+{
+    private readonly HashSet<GameObject> mapGo = new HashSet<GameObject>();
+    private readonly HashSet<GameObject> itemGo = new HashSet<GameObject>();
+
+    public void SaveMap()
+    {
+
+    }
+
+    public void LoadMap()
+    {
+
+    }
+}
