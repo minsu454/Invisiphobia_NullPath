ea25a030ae2d92975ef6b3f5c52cdd070a2f8b9a
keubung <skdud3390@naver.com>
2024-12-03 18:02:34 +0900

    [Add] Battery

---
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Prop/Item/InteractableItem/Battery.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : InteractableItem
+{
+    [Header("Battery Settings")]
+    public float dischargeRate = 1f; // 초당 감소량
+    private float maxCharge = 100f; // 배터리의 최대 충전량
+
+    [SerializeField] private float currentCharge; // 현재 배터리 충전량
+
+    public override void Interact(Player player)
+    {
+    }
+
+    void Start()
+    {
+        currentCharge = maxCharge; // 초기 충전량 설정
+        StartCoroutine(DischargeBattery());
+    }
+
+    private IEnumerator DischargeBattery()
+    {
+        while (currentCharge > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            currentCharge -= dischargeRate;
+            currentCharge = Mathf.Clamp(currentCharge, 0, maxCharge);
+        }
+    }
+}
