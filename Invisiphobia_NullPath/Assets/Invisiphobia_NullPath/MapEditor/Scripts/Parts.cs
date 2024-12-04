b0387a61c046ef0853f576b2e9829b3acb9df380
minsu454 <kminsu454@gmail.com>
2024-12-04 10:24:36 +0900

    [Fix] RoomEditor Material
    에디터 머터리얼 저장 오류 수정 및 Parts -> IParts로 전환

--- Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/MapEditor/Scripts/Parts.cs
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/MapEditor/Scripts/Parts.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class Parts : MonoBehaviour
 {
-    [SerializeField] private Vector3Int size;
-    public Vector3Int Size { get { return size; } }
+    
 }
 
---
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Interface/IParts.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+/// <summary>
+/// 부품 인터페이스
+/// </summary>
+public interface IParts
+{
+    public GameObject gameObject { get; }
+    public Transform transform { get; }
+}
