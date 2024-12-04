c5c4d53e617cd6e7320d120253db493a023f665f
keubung <skdud3390@naver.com>
2024-12-04 20:09:48 +0900

    [Update] ItemImage Name

--- Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Prop/Item/InteractableItem/FallingObstacleObject.cs
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Prop/Item/InteractableItem/FallingObstacleObject.cs
@@ -5,12 +5,7 @@ using UnityEngine.AI;
 
 public class FallingObstacleObject : InteractableItem
 {
-    [Header("Obstacle Settings")]
-    public Collider obstacleCollider;
-    public NavMeshObstacle navMeshObstacle;
-
     private bool isFallen = false;
-    public bool test = false;
     [SerializeField] private bool isDestroyed = false;
 
     public override void Interact(Player player)
