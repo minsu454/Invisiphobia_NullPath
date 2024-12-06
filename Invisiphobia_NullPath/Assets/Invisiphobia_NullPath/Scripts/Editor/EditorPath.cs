cd7578ae739028edbe668c0b144ae3b404487c8a
moloch-kim <79761359+moloch-kim@users.noreply.github.com>
2024-12-06 10:28:55 +0900

    [Rename] Foldering Adjust
    파츠 폴더 이름 정렬, 룸파츠들 수정

--- Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Editor/EditorPath.cs
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Editor/EditorPath.cs
@@ -5,11 +5,11 @@ public static class EditorPath
     public const string planePath = "Assets/Invisiphobia_NullPath/MapEditor/Plane/Plane.prefab";    //바닥 격자프리팹
 
     public const string roomTexturePath = "Assets/Invisiphobia_NullPath/MapEditor/Image/Room";      //roomParts이미지 경로
-    public const string roomPartsPath = "Assets/Invisiphobia_NullPath/MapEditor/RoomParts";         //roomPartsGO 경로
-    public const string decorTexturePath = "Assets/Invisiphobia_NullPath/MapEditor/Image/Decor";    //DecorParts이미지 경로
-    public const string decorPartsPath = "Assets/Invisiphobia_NullPath/MapEditor/DecorParts";       //DecorPartsGO 경로
+    public const string roomPartsPath = "Assets/Invisiphobia_NullPath/MapEditor/Parts_Room";        //roomPartsGO 경로
+    public const string decorTexturePath = "Assets/Invisiphobia_NullPath/MapEditor/Image/Deco";     //DecorParts이미지 경로
+    public const string decorPartsPath = "Assets/Invisiphobia_NullPath/MapEditor/Parts_Deco";       //DecorPartsGO 경로
     public const string itemTexturePath = "Assets/Invisiphobia_NullPath/MapEditor/Image/Item";      //itemParts이미지 경로
-    public const string itemPartsPath = "Assets/Invisiphobia_NullPath/MapEditor/ItemParts";         //itemPartsGO 경로
+    public const string itemPartsPath = "Assets/Invisiphobia_NullPath/MapEditor/Parts_Item";        //itemPartsGO 경로
 
     public const string materialPath = "Assets/Invisiphobia_NullPath/MapEditor/Material";           //Material 경로
 }
