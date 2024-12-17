313726fd4d4aa1b2f1d0813edb2dbca38e593f01
leecoading <leehy011011@gmail.com>
2024-12-17 09:58:04 +0900

    [Update] Table Prefab, Dummy

--- Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Deco/Dummy.cs
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Deco/Dummy.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Dummy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform player;
+    public bool followWatch = false;
+
+    public Camera playerCamera;
+    public float fieldOfViewAngle = 55f;
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+            followWatch = true;
     }
 
-    // Update is called once per frame
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            followWatch = false;
+    }
+
     void Update()
     {
-        
+        if (followWatchWhenBehindPlayer())
+        {
+            // 플레이어가 마네킹을 보고 있으면 따라보지 않음
+            return;
+        }
+        followWatchPlayer();
+    }
+
+    void followWatchPlayer()
+    {
+        if (!followWatch || player == null)
+            return;
+
+        Vector3 direction = player.position - transform.position;
+        direction.y = 0; // Y축 고정
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+
+    bool followWatchWhenBehindPlayer()
+    {
+        // 마네킹의 위치에서 플레이어 카메라의 방향으로 벡터 계산
+        Vector3 directionToDummy = (transform.position - playerCamera.transform.position).normalized;
+        float angle = Vector3.Angle(playerCamera.transform.forward, directionToDummy);
+
+        return angle <= fieldOfViewAngle;
     }
 }
