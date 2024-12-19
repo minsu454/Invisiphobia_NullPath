46364ac88db413f8c80ed484462c702f0f90bce8
moloch-kim <79761359+moloch-kim@users.noreply.github.com>
2024-12-19 14:58:19 +0900

    [Update] Prefabs setting

--- Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Editor/EditorPath.cs
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Editor/EditorPath.cs
@@ -1,6 +1,6 @@
 public static class EditorPath
 {
-    public const string UseScenePath = "Assets/Invisiphobia_NullPath/Scenes/MapEditor.unity";       //전용씬 경로
+    public const string UseScenePath = "Assets/Invisiphobia_NullPath/Scenes/MapEditor/MapEditor.unity";       //전용씬 경로
 
     public const string PlanePath = "Assets/Invisiphobia_NullPath/MapEditor/Plane/Plane.prefab";    //바닥 격자프리팹
 
