9d61d091bd2bf67c3f4d9c97b695bfeb92763a90
keubung <skdud3390@naver.com>
2024-12-23 11:16:25 +0900

    [Update] BossMonsterController.cs
    summary 추가

--- Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Entity/Monster/BossMonsterController.cs
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Entity/Monster/BossMonsterController.cs
@@ -10,6 +10,9 @@ public class BossMonsterController : MonsterController
     Coroutine coroutine;
     private const float attackDistance = 3f;
 
+    /// <summary>
+    /// 몬스터의 상태를 초기화하고 타겟을 플레이어로 설정
+    /// </summary>
     public override void Init(Monster monster)
     {
         base.Init(monster);
@@ -28,6 +31,9 @@ public class BossMonsterController : MonsterController
     {
     }
 
+    /// <summary>
+    /// NavMesh에 유효한 타겟 위치 업데이트
+    /// </summary>
     protected override void AttackingUpdate()
     {
         if (!agent.enabled)
@@ -41,6 +47,9 @@ public class BossMonsterController : MonsterController
         }
     }
 
+    /// <summary>
+    /// 콜라이더 충돌 중 플레이어와의 거리 계산
+    /// </summary>
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -49,6 +58,9 @@ public class BossMonsterController : MonsterController
         }
     }
 
+    /// <summary>
+    /// 콜라이더에서 빠져나오면 코루틴을 중지
+    /// </summary>
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -60,6 +72,9 @@ public class BossMonsterController : MonsterController
         }
     }
 
+    /// <summary>
+    /// 플레이어가 공격 범위 내에 들어오면 게임 오버
+    /// </summary>
     IEnumerator CoTargetDistance()
     {
         while (true)
@@ -73,11 +88,17 @@ public class BossMonsterController : MonsterController
         }
     }
 
+    /// <summary>
+    /// 몬스터 상태를 항상 Attacking으로 업데이트
+    /// </summary>
     void OnAttackingUpdate()
     {
         monster.aiState = AIStateType.Attacking;
     }
 
+    /// <summary>
+    /// 플레이어를 바라보도록 하는 함수
+    /// </summary>
     protected override void LookingAtTarget()
     {
         Vector3 directionToPlayer = (targetTransform.position - transform.position).normalized;
