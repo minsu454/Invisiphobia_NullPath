1b578139644f5e186c7528fbaac9164673a30843
keubung <skdud3390@naver.com>
2024-12-24 14:18:25 +0900

    [Update] FallingObstacleObject
    Board
    실행X

--- Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Prop/Item/InteractableItem/FallingObstacleObject.cs
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Prop/Item/InteractableItem/FallingObstacleObject.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class FallingObstacleObject : BaseItem
 {
+    public Transform forcePosition; // 힘을 가할 위치
+    public float forceAmount = 500f; // 힘의 크기
+    private Rigidbody rb;
     private bool isFallen = false;
     [SerializeField] private bool isDestroyed = false;
 
@@ -10,6 +13,11 @@ public class FallingObstacleObject : BaseItem
     {
         base.Init();
         interactText = DataService.GetItemInteractText(ItemTable.interactText[0]);
+
+        rb = GetComponent<Rigidbody>();
+
+        rb.isKinematic = true;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
     }
 
     public override void Interact(Player player)
@@ -17,8 +25,16 @@ public class FallingObstacleObject : BaseItem
         if (isFallen)
             return;
 
+        rb.isKinematic = false;
+        rb.constraints = RigidbodyConstraints.None;
+
+        Vector3 forceDirection = forcePosition.right;
+        rb.AddForceAtPosition(forceDirection * forceAmount, forcePosition.position, ForceMode.Impulse);
+
         isFallen = true;
-        if(isDestroyed)
+        rb.isKinematic = true;
+
+        if (isDestroyed)
             DestroyObstacle();
     }
 
