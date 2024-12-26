aaa2be085dff92af968e5116c726b95ca9b75e24
keubung <skdud3390@naver.com>
2024-12-26 12:36:13 +0900

    [Update] BossMonsterSound
    제거 후 다시 추가

--- Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Entity/Monster/CowardMonster.cs
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Entity/Monster/CowardMonster.cs
@@ -3,6 +3,8 @@ using UnityEngine.AI;
 
 public class CowardMonster : MonsterController
 {
+    [SerializeField] private Transform targetPosition;
+
     public float fadeDuration = 2f;
     public float fleeDuration = 4f;
 
@@ -31,6 +33,12 @@ public class CowardMonster : MonsterController
         }
     }
 
+    // SetTargetDestination 사용
+    // 감지된 후 이동하다가 플레이어의 시야에서 1초동안 감지되지 않으면 그 자리에 멈춰서 다시 투명화 - 감지될 수 있고 감지되면 다시 위치로 이어서 이동
+    // fade는 마지막에 위치에 도착하면 멈춰서 fade
+    /// <summary>
+    /// 도망 조건을 만족하며 도망 위치로 이동하는 함수
+    /// </summary>
     void MonsterFleeingUpdate()
     {
         float elapsedTime = Time.time - fleeStartTime;
@@ -66,6 +74,22 @@ public class CowardMonster : MonsterController
         if (NavMesh.SamplePosition(randomDirection, out NavMeshHit hit, maxDistance, NavMesh.AllAreas))
         {
             agent.SetDestination(hit.position);
+        }
+    }
+
+    /// <summary>
+    /// 지정해준 포지션으로 이동하는 함수
+    /// </summary>
+    void SetTargetDestination() // 이거로 사용
+    {
+        float maxPosition = 1.0f;
+
+        if (targetPosition != null)
+        {
+            if (NavMesh.SamplePosition(targetPosition.position, out NavMeshHit hit, maxPosition, NavMesh.AllAreas))
+            {
+                agent.SetDestination(hit.position);
+            }
         }
     }
 
