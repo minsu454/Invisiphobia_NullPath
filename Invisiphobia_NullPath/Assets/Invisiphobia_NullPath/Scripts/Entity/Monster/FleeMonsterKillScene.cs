37d279315156c2e914365ce6aa84ed73a9c10080
keubung <skdud3390@naver.com>
2025-01-08 19:54:33 +0900

    [Update] MonsterAnim

--- Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Entity/Monster/FleeMonsterKillScene.cs
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Entity/Monster/FleeMonsterKillScene.cs
@@ -9,7 +9,6 @@ public class FleeMonsterKillScene : MonsterKillScene
 {
     protected override void Kill()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
         GetComponent<Rigidbody>().isKinematic = true;
         if (monster.StateType != PropStateType.Revealed)
         {
@@ -29,6 +28,7 @@ public class FleeMonsterKillScene : MonsterKillScene
 
     IEnumerator CoUnrevealKillAnimTime()
     {
+        transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
         yield return YieldCache.WaitForSeconds(2f);
         target.Die();
     }
