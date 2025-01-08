76abb3ea8d0a46cf5fc96e7956de3bad439eed2d
keubung <skdud3390@naver.com>
2025-01-08 12:19:46 +0900

    [Update] Runmaul Anim

--- Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Entity/Monster/FleeMonsterKillScene.cs
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Entity/Monster/FleeMonsterKillScene.cs
@@ -1,4 +1,5 @@
 using Common.Event;
+using Common.Yield;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,6 +9,27 @@ public class FleeMonsterKillScene : MonsterKillScene
 {
     protected override void Kill()
     {
+        transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+        GetComponent<Rigidbody>().isKinematic = true;
+        if (monster.StateType != PropStateType.Revealed)
+        {
+            StartCoroutine(CoUnrevealKillAnimTime());
+        }
+        else
+        {
+            StartCoroutine(CoKillAnimTime());
+        }
+    }
+
+    IEnumerator CoKillAnimTime()
+    {
+        yield return YieldCache.WaitForSeconds(2f);
+        target.Die();
+    }
+
+    IEnumerator CoUnrevealKillAnimTime()
+    {
+        yield return YieldCache.WaitForSeconds(2f);
         target.Die();
     }
 }
