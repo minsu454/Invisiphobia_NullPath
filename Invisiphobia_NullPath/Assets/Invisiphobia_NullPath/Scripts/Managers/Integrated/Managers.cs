6a5b98c682df513d909317e0c0261215b051f566
minsu454 <kminsu454@gmail.com>
2025-01-14 18:58:59 +0900

    [Update] Language
    언어적용

--- Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Managers/Integrated/Managers.cs
+++ Invisiphobia_NullPath/Assets/Invisiphobia_NullPath/Scripts/Managers/Integrated/Managers.cs
@@ -27,6 +27,7 @@ public sealed class Managers : MonoBehaviour
         instance.uiManager = CreateManager<UIManager>(go.transform);
         instance.soundManager = CreateManager<SoundManager>(go.transform);
 
+        DataService.Init();
         SceneJobLoader.Init();
         SettingManager.Init();
     }
